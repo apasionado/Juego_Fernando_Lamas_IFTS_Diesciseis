2a753b2d54fa92186bfb05bf6cbb1adbcd1ee3b9
apasionado <35930141+apasionado@users.noreply.github.com>
2018-10-18 14:38:15 -0300

    Creación de clases básicas clasificadas por paquetes

---
+++ src/juego_fernando_lamas_ifts_diesciseis/Entidades/EntidadesHumanas.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package juego_fernando_lamas_ifts_diesciseis.Entidades;
+
+/**
+ *
+ * @author Fernando Lamas
+ */
+public class EntidadesHumanas {
+    /*el humano dice hace piensa y apartir de eso interactua con el sistema
+    
+    si es necesario revisar el doc "HumanEntity"
+    */
+}
---
+++ src/juego_fernando_lamas_ifts_diesciseis/Entidades/EntidadesVivas.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package juego_fernando_lamas_ifts_diesciseis.Entidades;
+
+/**
+ *
+ * @author Fernando Lamas
+ */
+public class EntidadesVivas {
+    
+    /* Las entidades vivas vendrian a ser mas los NPC que se desarrollan en el juego
+    a medida que el usuario va interactuando con ellos el NPC responde a cada interactuación
+    
+    */
+    
+    
+}
