b0d201ac2c216bd261672751440abd62dea5e55a
apasionado <35930141+apasionado@users.noreply.github.com>
2018-10-18 15:13:50 -0300

    Implementación del modelo prototipo a seguir
    Hay que decidir si será usado o no en un futuro en base a la idea proyectada

---
+++ src/juego_fernando_lamas_ifts_diesciseis/Eventos/Player.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package juego_fernando_lamas_ifts_diesciseis.Eventos;
+
+/**
+ *
+ * @author Fernando Lamas
+ */
+public class Player {
+    
+}
---
+++ src/juego_fernando_lamas_ifts_diesciseis/Mundo/Prueba.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package juego_fernando_lamas_ifts_diesciseis.Mundo;
+
+/**
+ *
+ * @author Fernando Lamas
+ */
+public class Prueba {
+    
+    
+    /* este es un mundo de prueba*/
+    
+    
+}
